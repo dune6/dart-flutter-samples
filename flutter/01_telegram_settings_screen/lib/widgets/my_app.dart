2dc122ac09661511ef3a9c2b3255898cb53ef336
dune6 <ilya.voznesensky@gmail.com>
2022-01-29 21:23:17 +0300

    Complete lesson 21

--- flutter/01_telegram_settings_screen/lib/widgets/my_app.dart
+++ flutter/01_telegram_settings_screen/lib/widgets/my_app.dart
@@ -6,10 +6,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: UserProfile(
-
-      ),
+    return  MaterialApp(
+      home: UserProfile(),
     );
   }
 }
--- flutter/01_telegram_settings_screen/lib/widgets/user_profile.dart
+++ flutter/01_telegram_settings_screen/lib/widgets/user_profile.dart
@@ -1,13 +1,29 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class UserProfile extends StatelessWidget {
-  const UserProfile();
+  UserProfile();
+
+  final List<MenuRowData> fristMenuRow = [
+    MenuRowData(Icons.favorite, 'Избранное'),
+    MenuRowData(Icons.phone, 'Недавние звонки'),
+    MenuRowData(Icons.computer, 'Устройства'),
+    MenuRowData(Icons.folder, 'Папка с чатами'),
+  ];
+
+  final List<MenuRowData> secondMenuRow = [
+    MenuRowData(Icons.notifications, 'Уведомления'),
+    MenuRowData(Icons.security, 'Конфиденциальность'),
+    MenuRowData(Icons.storage, 'Данные и память'),
+    MenuRowData(Icons.format_paint, 'Оформление'),
+    MenuRowData(Icons.language, 'Язык'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('Настройки'),
       ),
       body: SizedBox(
@@ -15,6 +31,85 @@ class UserProfile extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const _UserInfo(),
+            const SizedBox(height: 20,),
+            _MenuWidget(
+              menuRow: fristMenuRow,
+            ),
+            const SizedBox(height: 20,),
+            _MenuWidget(menuRow: secondMenuRow)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MenuRowData {
+  final IconData icon;
+  final String text;
+
+  MenuRowData(this.icon, this.text);
+}
+
+class _MenuWidget extends StatelessWidget {
+  final List<MenuRowData> menuRow;
+
+  const _MenuWidget({Key? key, required this.menuRow}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      child: Column(
+        children: menuRow.map((data) => _MenuWidgetRow(data: data)).toList(),
+      ),
+    );
+  }
+}
+
+class _MenuWidgetRow extends StatelessWidget {
+  final MenuRowData data;
+
+  const _MenuWidgetRow({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Icon(data.icon),
+          const SizedBox(
+            width: 10,
+          ),
+          Expanded(child: Text(data.text)),
+          const Icon(Icons.chevron_right),
+        ],
+      ),
+    );
+  }
+}
+
+class _UserInfo extends StatelessWidget {
+  const _UserInfo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 5),
+        child: Column(
           children: const [
             SizedBox(
               height: 20,
